06860dffbf0c8470e9ce4ecd78f19b70540b054d
tanem <tane.morgan@gmail.com>
2015-11-26 14:01:03 +1300

    Add router tests (fixes #8)

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -1,13 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import routes from '../routes';
+import createHistory from 'history/lib/createBrowserHistory';
+import configureRoutes from '../routes';
+import App from './App';
 
 export default class Root extends Component {
   render() {
     const { store } = this.props;
     return (
       <Provider store={store}>
-        {routes}
+        {configureRoutes(createHistory(), App)}
       </Provider>
     );
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Router, Route } from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-import App from '../containers/App';
 
-const history = createBrowserHistory();
-
-export default (
-  <Router history={history}>
-    <Route path="/" component={App} />
-  </Router>
-);
+export default function configureRoutes(history, App) {
+  return (
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  );
+}
